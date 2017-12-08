c3b248ed6ed1b5ddf40870bd763e3b3d7adb405b
Luís Ferreira <lsferreira169@gmail.com>
2017-12-08 04:24:50 -0800

    Add compatibility for macOS

--- FreeImageConfig.cmake
+++ FreeImageConfig.cmake
@@ -25,38 +25,38 @@
 # FreeImage_FOUND, system has FreeImage
 
 if(NOT DEFINED FREEIMAGE_MODULE_LOADED)
-set(FREEIMAGE_MODULE_LOADED true)
-IF(NOT DEFINED AURORAFW_IS_BUILDING)
-	find_package(AuroraFWBuild REQUIRED)
-ENDIF()
+	set(FREEIMAGE_MODULE_LOADED true)
+	IF(NOT DEFINED AURORAFW_IS_BUILDING)
+		find_package(AuroraFWBuild REQUIRED)
+	ENDIF()
 
-findpkg_begin(FreeImage)
+	findpkg_begin(FreeImage)
 
-getenv_path(FREEIMAGE_HOME)
+	getenv_path(FREEIMAGE_HOME)
 
-set(FreeImage_PREFIX_PATH ${FREEIMAGE_HOME} ${ENV_FREEIMAGE_HOME})
-create_search_paths(FreeImage)
+	set(FreeImage_PREFIX_PATH ${FREEIMAGE_HOME} ${ENV_FREEIMAGE_HOME})
+	create_search_paths(FreeImage)
 
-clear_if_changed(FreeImage_PREFIX_PATH
-	FreeImage_LIBRARY_FWK
-	FreeImage_LIBRARY_REL
-	FreeImage_LIBRARY_DBG
-	FreeImage_INCLUDE_DIR
-)
+	clear_if_changed(FreeImage_PREFIX_PATH
+		FreeImage_LIBRARY_FWK
+		FreeImage_LIBRARY_REL
+		FreeImage_LIBRARY_DBG
+		FreeImage_INCLUDE_DIR
+	)
 
-set(FreeImage_LIBRARY_NAMES freeimage freeimageLib FreeImage FreeImageLib)
-get_debug_names(FreeImage_LIBRARY_NAMES)
+	set(FreeImage_LIBRARY_NAMES freeimage freeimageLib FreeImage FreeImageLib)
+	get_debug_names(FreeImage_LIBRARY_NAMES)
 
-findpkg_framework(FreeImage)
+	findpkg_framework(FreeImage)
 
-find_path(FreeImage_INCLUDE_DIR NAMES FreeImage.h HINTS ${FreeImage_INC_SEARCH_PATH} ${FreeImage_PKGC_INCLUDE_DIRS})
+	find_path(FreeImage_INCLUDE_DIR NAMES FreeImage.h HINTS ${FreeImage_INC_SEARCH_PATH} ${FreeImage_PKGC_INCLUDE_DIRS})
 
-find_library(FreeImage_LIBRARY_REL NAMES ${FreeImage_LIBRARY_NAMES} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Release RelWithDebInfo MinSizeRel)
-find_library(FreeImage_LIBRARY_DBG NAMES ${FreeImage_LIBRARY_NAMES_DBG} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Debug)
+	find_library(FreeImage_LIBRARY_REL NAMES ${FreeImage_LIBRARY_NAMES} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Release RelWithDebInfo MinSizeRel)
+	find_library(FreeImage_LIBRARY_DBG NAMES ${FreeImage_LIBRARY_NAMES_DBG} HINTS ${FreeImage_LIB_SEARCH_PATH} ${FreeImage_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Debug)
 
-make_library_set(FreeImage_LIBRARY)
+	make_library_set(FreeImage_LIBRARY)
 
-findpkg_finish(FreeImage)
+	findpkg_finish(FreeImage)
 
 endif(NOT DEFINED FREEIMAGE_MODULE_LOADED)
 ################################################################################
